9a7f38d06751a83c673b9d555f2b4905e27a3397
Lashji <lassi.palojarvi@gmail.com>
2017-12-05 16:55:26 +0200

    learning project v1

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const http = require('http');
+const app = express();
+
+const api = require("./server/routes/api");
